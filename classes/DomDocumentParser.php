84196439f0af692a4257b51d02febf5a0e363aec
Shaila Sabrin <shaila.sabrin.23@gmail.com>
2019-03-10 19:44:31 -0700

    Setup project directory as git repo

---
+++ classes/DomDocumentParser.php
@@ -0,0 +1,12 @@
+<?php
+class DomDocumentParser{
+	public function __construct($url){
+
+		$option = array()
+
+
+
+	}
+}
+
+?>
---
+++ search.php
@@ -0,0 +1,80 @@
+<?php
+
+ //base case
+ if (isset($_GET["term"])){
+ 	$term = $_GET["term"];
+ }
+ else{
+
+ 	exit ("You must enter a search term");
+ }
+  
+// if type is not mentioned, default to "sites"
+$type = isset($_GET["type"]) ? $_GET["type"] :"sites";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Chirrup</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class= "wrapper">
+
+		<div class ="header">
+			<div class= "headerContent">
+				<div class="logoContainer">
+					<a href="index.php">
+					    <img src="assets/images/festisite_google.png">
+					</a>
+
+			    </div>
+
+			    <div class="searchContainer">
+			    	<form action ="serch.php" method ="GET">
+
+			    		<div class= "searchBarContainer">
+			    			<input class="searchBox" type="text" name="term">
+			    			<button class= "searchButton">
+			    				<img src="assets/images/icons/search.png">
+			    			</button>
+
+			    			
+			    		</div>
+			    		
+			    	</form>
+			    	
+			    </div>
+				
+			</div>
+
+
+			<div class="tabsContainer">
+				<ul class= "tabList">
+					<li class="<?php echo $type== 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li>
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+					
+					
+				</ul>
+				
+
+			</div>
+			
+		</div>
+		
+	</div>
+
+</body>
+</html>
